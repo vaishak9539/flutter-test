c4497aa095b709fa7641a24b82e45e5753fa3804
vaishak9539 <vaishakp17@gmail.com>
2024-12-21 11:41:21 +0530

    commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertest/widgets/custom_text.dart';
+import 'package:fluttertest/widgets/custom_textfield.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -13,8 +15,23 @@ class _LoginState extends State<Login> {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          
+          CustomText(text: "Sign in"),
+          CustomTextField(
+            hintText: "Email",
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          CustomTextField(
+            hintText: "Email",
+          ),
+          SizedBox(
+            height: 10,
+          ),
+           ElevatedButton(
+              onPressed: () => null, child: CustomText(text: "Login"))
         ],
       ),
     );
